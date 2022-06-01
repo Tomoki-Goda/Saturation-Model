9c58c45810a99de768ef65c0981d45a691560161
tomoki <tgoda@ifj.edu.pl>
2022-06-02 00:06:46 +0200

    fatal error with logs fixed

--- saturation30/new_func/all.sh
+++ saturation30/new_func/all.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 gcc Automation_Run.c -o Auto_Run
-gcc Auto-Plot-DP.c -o Auto-Plot.c
+gcc Auto-Plot-DP.c -o Auto-Plot
 ##########################################################
 ./Auto_Run ./GBS/*LCB 
 ./makelist 7 ./GBS/*massless*/result.txt ./GBS/massless.txt

861e9936610242b504b8870af165b7c8d047ca7b
Tomoki Goda <tgoda@ifj.edu.pl>
2022-08-10 21:05:47 +0200

    rfix nolonger an option

--- saturation-ver2/make-control.sh
+++ saturation-ver2/make-control.sh
@@ -5,22 +5,18 @@ RUNDIR=../Run0208
 rm -r ${RUNDIR}/*/*
 
 
-for i in GBW GBWS GBWS-Fix-S GBWS-Fix-C GBWS-Fix-rmax GBWSnp-Fix-S BGK BGKS-Fix-S; do mkdir ${RUNDIR}/${i} ; done 
+for i in GBW GBWS-Fix-S BGK BGKS-Fix-S; do mkdir ${RUNDIR}/${i} ; done 
 
- ./Auto-Control -dir ${RUNDIR}/GBW -rfix 0 -sudakov 0 -lmass 0.0 0.0196 -qup 100 650 -model 0
- ./Auto-Control -dir ${RUNDIR}/GBWS -rfix 0 -sudakov 1 -lmass 0.0 0.0196 -qup 100 650 -model 22
- ./Auto-Control -dir ${RUNDIR}/GBWS-Fix-S -rfix 0 -sudakov 1 -lmass 0.0 0.0196 -qup 100 650 -model 22
- ./Auto-Control -dir ${RUNDIR}/GBWSnp-Fix-S -rfix 0 -sudakov 2 -lmass 0.0 0.0196 -qup 100 650 -model 22
- ./Auto-Control -dir ${RUNDIR}/GBWS-Fix-C -rfix 0 -sudakov 1 -lmass 0.0 0.0196 -qup 100 650 -model 22
- ./Auto-Control -dir ${RUNDIR}/GBWS-Fix-rmax -rfix 0 -sudakov 2 -lmass 0.0 0.0196 -qup 100 650 -model 22
+ ./Auto-Control -dir ${RUNDIR}/GBW -sudakov 0 -lmass 0.0 0.0196 -qup 100 650 -model 0
+ ./Auto-Control -dir ${RUNDIR}/GBWS-Fix-S -sudakov 1 -lmass 0.0 0.0196 -qup 100 650 -model 22
 
- ./Auto-Control -dir ${RUNDIR}/BGKS-Fix-S -rfix 0 -sudakov 1 -lmass 0.0 0.0196 -qup 100 650 -model 3
- ./Auto-Control -dir ${RUNDIR}/BGK -rfix 0 -sudakov 0 -lmass 0.0 0.0196 -qup 100 650 -model 1
+ ./Auto-Control -dir ${RUNDIR}/BGK -sudakov 0 -lmass 0.0 0.0196 -qup 100 650 -model 1
+ ./Auto-Control -dir ${RUNDIR}/BGKS-Fix-S -sudakov 1 -lmass 0.0 0.0196 -qup 100 650 -model 3
 
 
  ./Append "#define FEJER 1" ${RUNDIR}/*/M* 
-# ./Append "#define INDEPENDENT_RMAX 1" ${RUNDIR}*/BGKS*/M*
- ./Append "#define N_CHEB_R 80" ${RUNDIR}/*/M* 
+ ./Append "#define INDEPENDENT_RMAX 1" ${RUNDIR}*/BGKS*/M*
+ ./Append "#define N_CHEB_R 100" ${RUNDIR}/*/M* 
 # ./Append "#define N_SIMPS_R 50" ${RUNDIR}/*/M* 
 
 #/////////////////////////////////////////////////////////////////////////////
@@ -41,33 +37,33 @@ do
 	make
 done
 
-for i in ${RUNDIR}*/GBWS-Fix-C/M*
-do 
-	cp ./minuit-strategy-gbws-fix-c.h ${i}/minuit-run.h
-	DIR=${i}
-	make
-done
+#for i in ${RUNDIR}*/GBWS-Fix-C/M*
+#do 
+#	cp ./minuit-strategy-gbws-fix-c.h ${i}/minuit-run.h
+#	DIR=${i}
+#	make
+#done
 
-for i in ${RUNDIR}*/GBWS-Fix-rmax/M*
-do 
-	cp ./minuit-strategy-gbws-fix-rmax.h ${i}/minuit-run.h
-	DIR=${i}
-	make
-done
+#for i in ${RUNDIR}*/GBWS-Fix-rmax/M*
+#do 
+#	cp ./minuit-strategy-gbws-fix-rmax.h ${i}/minuit-run.h
+#	DIR=${i}
+#	make
+#done
 
-for i in ${RUNDIR}*/GBWS/M*
-do 
-	cp ./minuit-strategy-gbws.h ${i}/minuit-run.h
-	DIR=${i}
-	make
-done
+#for i in ${RUNDIR}*/GBWS/M*
+#do 
+#	cp ./minuit-strategy-gbws.h ${i}/minuit-run.h
+#	DIR=${i}
+#	make
+#done
 
-for i in ${RUNDIR}*/GBWSnp-Fix-S/M*
-do 
-	cp ./minuit-strategy-gbws-fix-s.h ${i}/minuit-run.h
-	DIR=${i}
-	make
-done
+#for i in ${RUNDIR}*/GBWSnp-Fix-S/M*
+#do 
+#	cp ./minuit-strategy-gbws-fix-s.h ${i}/minuit-run.h
+#	DIR=${i}
+#	make
+#done
 
 
 
--- saturation-ver2/plot.sh
+++ saturation-ver2/plot.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 #for dir in ../Saturation-Model2207/Run2207*/*/M* # ../Results/BGK/M* ../Results/GBWS/M* ../Results/BGKS/M*
-for dir in ../Run2207/GBW*/M* 
+for dir in ../Run2607-Fejer/*/M* 
 do
-	for x in 3 5
+	for x in 3 5 7
 	do
-		#for q2 in 100 650
-		#do		
-		#	${dir}/dipole -in ${dir}/result.txt -out ${dir}/dipole-${q2}-${x}.txt -x ${x} -Q2 ${q2}
-		#done
+		for q2 in 100 650
+		do		
+			${dir}/dipole -in ${dir}/result.txt -out ${dir}/dipole-${q2}-${x}.txt -x ${x} -Q2 ${q2}
+		done
 		
 		${dir}/F2 -in ${dir}/result.txt -out ${dir}/F2-${x}.txt -x ${x}
-		#${dir}/F2-slope -in ${dir}/result.txt -out ${dir}/F2-slope-${x}.txt -x ${x}
+		${dir}/F2-slope -in ${dir}/result.txt -out ${dir}/F2-slope-${x}.txt -x ${x}
 			
 	done
 	for q2 in 100 650

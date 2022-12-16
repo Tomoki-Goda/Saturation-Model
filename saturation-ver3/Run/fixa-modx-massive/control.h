c5bd0bc0ee9ad4f7bb7cdf922885d1cb8cdf2dde
Tomoki Goda <tgoda@ifj.edu.pl>
2022-12-16 16:48:09 +0100

    gauss and clenshaw combined

---
+++ saturation-ver3/Run/fixa-modx-massive/control.h
@@ -0,0 +1,13 @@
+#define MASS_L2 0.0196
+#define MASS_S2 0.0196
+#define Q2_MAX 650.0
+#define MODEL 0
+#define SUDAKOV 0
+#define NONLINEAR 1
+#define FEJER 1
+#define N_CHEB_R 108
+#define DGAUSS_PREC 1.0e-9
+#define KT 1
+#define PRINT_PROGRESS 1
+#define ALPHA_RUN 0
+#define MODX 1

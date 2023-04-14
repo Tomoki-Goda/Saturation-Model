fc07c77a5f1d1ccacf0b4c3450963410ee0204e4
Tomoki Goda <tgoda@ifj.edu.pl>
2023-04-14 17:47:18 +0200

    restructuring

---
+++ Run-kt/fixa-bjorx/control.h
@@ -0,0 +1,10 @@
+#define MASS_L2 0.0
+#define MASS_S2 0.0
+#define Q2_MAX 650.0
+#define MODEL 0 
+#define PRINT_PROGRESS 1
+#define ALPHA_RUN 0
+#define MODX 0
+#define USE_RESULT 1 
+#define IBP 1
+#define GLUON_APPROX 1
